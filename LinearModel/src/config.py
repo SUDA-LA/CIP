851e82394d238e6639e563a825fe1b3bd222671b
shhuang <1004045950@qq.com>
2020-10-14 14:52:33 +0800

    update

--- LinearModel/src/config.py
+++ LinearModel/src/config.py
@@ -5,9 +5,9 @@ class config:
         self.iterations=50#最大迭代次数
         self.max_iterations=10#迭代多少次没有提升就退出
         if(isbigdata):
-            self.train="./big_data/train"
-            self.test="./big_data/test"
-            self.dev="./big_data/dev"
+            self.train="../big_data/train"
+            self.test="../big_data/test"
+            self.dev="../big_data/dev"
         else:
-            self.train="./small_data/train.conll"
-            self.dev="./small_data/dev.conll"
+            self.train="../small_data/train.conll"
+            self.dev="../small_data/dev.conll"
